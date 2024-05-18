32eec39f492b72d634c28fd539b41865383e0450
Rudi Visser <me@rudi.dev>
2024-05-18 16:52:33 +0100

    Add Properties to ReaperRoute to support #1

--- src/Reaper/Attributes/ReaperRouteAttribute.cs
+++ src/Reaper/Attributes/ReaperRouteAttribute.cs
@@ -5,5 +5,12 @@ namespace Reaper.Attributes;
 [AttributeUsage(AttributeTargets.Class)]
 public class ReaperRouteAttribute : Attribute
 {
-    public ReaperRouteAttribute(string verb, [StringSyntax("Route")] string route) { }
+    public string Verb { get; }
+    public string Route { get; }
+    
+    public ReaperRouteAttribute(string verb, [StringSyntax("Route")] string route)
+    {
+        Verb = verb;
+        Route = route;
+    }
 }
